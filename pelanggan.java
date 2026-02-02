f75f276613e9ac19521839067941f100cf36c878
Zaki <Qhtbzaki@gmail.com>
2026-02-02 14:27:40 +0700

    commitbranch

--- pelanggan.java
+++ pelanggan.java
@@ -9,5 +9,6 @@ public class pelanggan {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("NaKI Forever");
+        System.out.println("ini branch zaki");
     }
 }
